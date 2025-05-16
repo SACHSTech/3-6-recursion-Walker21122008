e3f10dd5e17d3a6dc28473f71d0f06113e96d15e
hasini2k8 <hasini2k8@gmail.com>
2025-05-16 14:12:52 -0400

    still making generate subsets

--- src/recursion/generateSubsets.java
+++ src/recursion/generateSubsets.java
@@ -2,7 +2,20 @@ package recursion;
 
 public class generateSubsets {
     public static void generateSubsets(String s) {
-        System.out.println(s);
-        
+        generateSubsets(s, "", 0);
+    }
+
+    private static void generateSubsets(String s, String current, int index) {
+        if (index == s.length()) {
+            System.out.println(current);
+            return;
+        }
+
+        generateSubsets(s, current + s.charAt(index), index + 1);
+        generateSubsets(s, current, index + 1);
+    }
+
+    public static void main(String[] args) {
+        generateSubsets("abc");
     }
 }
